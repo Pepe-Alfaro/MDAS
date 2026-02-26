28908dbe255bb60de52f3cf14f6910d5dcce5717
i32maloe <i32maloe@uco.es>
2026-02-26 11:10:20 +0100

    Clase Plato

--- abstractfactory/Plato.java
+++ abstractfactory/Plato.java
@@ -2,7 +2,22 @@ package abstractfactory;
 
 public class Plato {
     private String nombre;
-    private TipoPlato type;
+    private TipoPlato tipo;
     private float precio;
+    private float incremento; 
+    private Acompanamiento side;
+
+    public Plato(String nombre, TipoPlato tipo, float precio, float incremento, Acompanamiento side){
+        nombre = this.nombre;
+        tipo = this.tipo;
+        precio = this.precio;
+        incremento = this.incremento;
+        side = this.side;
+    }
+
+    public String getName(){
+
+    }
     
+
 }
