98b3a1987d88ab5e718e957a484e6bc1211d47ef
i32chroc <i32chroc@uco.es>
2026-02-26 18:49:30 +0100

    Menu tipo

--- abstractfactory/Temporada.java
+++ abstractfactory/Temporada.java
@@ -1,5 +1,14 @@
 package abstractfactory;
 
-public class Temporada {
-    
+public class Temporada extends Menu {
+
+    public Temporada(Plato plato) {
+        super(plato);
+    }
+
+    @Override
+    public String toString() {
+        return "Menú de Temporada compuesto por: " + getPlatos().size() + " platos. " +
+               "Precio total: " + calcularPrecioTotal() + "€";
+    }
 }
