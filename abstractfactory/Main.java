c474b67c4d7cdf111700dd484759e802b98fcb71
Pepealfaro4 <i32algoj@uco.es>
2026-02-26 18:57:41 +0100

    Cambios en el proyecto de patrones de diseño.

---
+++ abstractfactory/Main.java
@@ -0,0 +1,27 @@
+package abstractfactory;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Probamos el pedido en el Restaurante (Local)
+        System.out.println("--- PEDIDO EN LOCAL ---");
+        FactoriaAbstracta factoryLocal = new FactoriaRestaurante();
+        Menu menuLocal = factoryLocal.crearMenuSemanal(Acompanamiento.PATATAS);
+        
+        imprimirMenu(menuLocal);
+
+        System.out.println("\n--- PEDIDO PARA LLEVAR ---");
+        // 2. Probamos el pedido Para Llevar (TakeAway)
+        FactoriaAbstracta factoryLlevar = new FactoriaTakeAway();
+        Menu menuLlevar = factoryLlevar.crearMenuSemanal(Acompanamiento.ENSALADA);
+        
+        imprimirMenu(menuLlevar);
+    }
+
+    public static void imprimirMenu(Menu menu) {
+        System.out.println("Platos incluidos:");
+        for (Plato p : menu.getPlatos()) {
+            System.out.println("- " + p.getNombre() + " (" + p.getTipo() + ") | Acompañamiento: " + p.getSide());
+        }
+        System.out.println("PRECIO TOTAL: " + menu.calcularPrecioTotal() + "€");
+    }
+}
