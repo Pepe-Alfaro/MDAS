2fb2db073cbe5f9333a8ae60c855a074e35b7948
i32chroc <i32chroc@uco.es>
2026-02-26 18:49:30 +0100

    Menu tipo

--- abstractfactory/Semanal.java
+++ abstractfactory/Semanal.java
@@ -1,6 +1,15 @@
 package abstractfactory;
 
+public class Semanal extends Menu {
 
-public class Semanal {
-    
+    // Constructor que recibe el primer plato
+    public Semanal(Plato plato) {
+        super(plato);
+    }
+
+    @Override
+    public String toString() {
+        return "Menú Semanal compuesto por: " + getPlatos().size() + " platos. " +
+               "Precio total: " + calcularPrecioTotal() + "€";
+    }
 }
