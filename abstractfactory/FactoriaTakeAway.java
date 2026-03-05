3c0c4d890156e3f2f93ae18fe017a12eebc104f9
i32chroc <i32chroc@uco.es>
2026-03-05 11:53:20 +0100

    TakeAway

--- abstractfactory/FactoriaTakeAway.java
+++ abstractfactory/FactoriaTakeAway.java
@@ -1,2 +1,37 @@
 package abstractfactory;
+import java.util.List;
 
+public class FactoriaTakeAway extends FactoriaAbstracta{
+
+public Semanal crearMenuSemanal(List<Plato>platos,Acompanamiento side){
+
+    for (Plato plato : platos){
+        if( plato.getTipo() == TipoPlato.PRINCIPAL ){
+            plato.setSide(side);
+        };
+        precio = plato.getPrecio()*1.02;
+        plato.setPrecio(precio);
+
+    }
+
+    Semanal menu = new Semanal(platos);   
+    return menu;
+
+
+}
+
+public Temporada crearMenuTemporada(List<Plato> platos, Acompanamiento side){
+
+    for(Plato plato : platos){
+        if(plato.getTipo() == TipoPlato.PRINCIPAL){
+            plato.setSide(side); 
+        }
+        precio = plato.getPrecio()*1.02;
+        plato.setPrecio(precio);
+    }
+    
+    Temporada menu = new Temporada(platos);
+    return menu;
+}
+
+}
