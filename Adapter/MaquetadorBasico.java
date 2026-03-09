ba802af36edca5701788cc67347a0aadcb2b76aa
i32maloe <i32maloe@uco.es>
2026-03-09 23:58:26 +0100

    Funciones clase Adapter

--- Adapter/MaquetadorBasico.java
+++ Adapter/MaquetadorBasico.java
@@ -1,20 +1,28 @@
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+
 
 public class MaquetadorBasico {
 
 
 
 public void AnadirTexto(File fichero,String texto){
+    boolean vacio = false;
+    
+    if(fichero.length() == 0){
+        vacio = true;
+    }
 
     try(FileWriter fich = new FileWriter(fichero,true)){
         
-        fich.write("\n"+texto);
+        if( vacio == false){
+        fich.write("\n"+texto.trim());
         fich.close();
+        
+        }else{ fich.write(texto.trim());}
 
     }catch (IOException e){
         System.out.println("Error en la funcion Añadir\n");
