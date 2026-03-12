33f35c32aeef5e7eb2df81636b9a5f3ae2889210
Pepealfaro4 <i32algoj@uco.es>
2026-03-12 10:56:06 +0100

    Patron Composite terminado.

--- composite/AparatoElectrico.java
+++ composite/AparatoElectrico.java
@@ -1,5 +1,18 @@
 package composite;
 
-public class AparatoElectrico {
+public class AparatoElectrico extends ComponenteEnergia
+{ 
+    private double consumoPorHora;
+    private int horasUso;
+
+    public AparatoElectrico(String nombre, double consumoPorHora, int horasUso) {
+        super(nombre);
+        this.consumoPorHora = consumoPorHora;
+        this.horasUso = horasUso;
+    }
     
+    @Override
+    public double calcularConsumo() {
+        return this.consumoPorHora * this.horasUso;
+    }
 }
--- composite/EspacioComposite.java
+++ composite/EspacioComposite.java
@@ -1,5 +1,30 @@
 package composite;
 
-public class EspacioComposite {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EspacioComposite extends ComponenteEnergia {
+    private List<ComponenteEnergia> componentes;
+
+    public EspacioComposite(String nombre) {
+        super(nombre);
+        this.componentes = new ArrayList<>();
+    }
+
+    public void agregarComponente(ComponenteEnergia componente) {
+        this.componentes.add(componente);
+    }
+    public void eliminarComponente(ComponenteEnergia componente) {
+        this.componentes.remove(componente);
+    }
+
+    @Override
+    public double calcularConsumo() {
+        double gastoTotal = 0;
+        for (ComponenteEnergia c : componentes) {
+            gastoTotal += c.calcularConsumo();
+        }
+        return gastoTotal;
+    }
     
 }
